dada8c33ed94753af5731434d246afb3de5db034
mannat244 <mecommunitymail@gmail.com>
2025-04-17 11:13:26 +0530

    cloud 8

--- portfolio-web/app/components/NavBar.js
+++ portfolio-web/app/components/NavBar.js
@@ -3,8 +3,8 @@ import React from 'react'
 const NavBar = ({scrollfunc , refs}) => {
   return (
     <div className='bg-zinc-900 shadow-lg shadow-black/30 h-14 flex justify-center items-center text-white '>
-      <img onClick={() => {window.location.href = "/"}} src='/vercel.svg' className='h-8 ml-5 mr-auto'/>
-      <div className='flex items-center justify-center font-sans gap-10 font-semibold mr-10'>
+      <img onClick={() => {window.location.href = "/"}} src='/rlogo.png' className=' h-10 ml-3 mr-auto'/>
+      <div className='flex ml-2 items-center justify-center font-sans gap-10 font-semibold mr-10'>
         <button onClick={() => scrollfunc(refs?.Heroref)}>Home</button>
         <button onClick={() => scrollfunc(refs?.Aboutref)}>Features</button>
         <button onClick={() => scrollfunc(refs?.Featuresref)}>How It Works</button>
